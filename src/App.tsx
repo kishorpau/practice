1c65e87709fe22f8970831498e83372c984c95cf
kishorpau <kishorpau66@gmail.com>
2024-10-16 19:54:04 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Button } from "@mui/material"
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained"
+      sx={{
+        bgcolor:"red",
+        marginTop:"5px"
+
+      }}>
+        HEllo World
+      </Button>
+    </div>
+  )
+}
+
+export default App
